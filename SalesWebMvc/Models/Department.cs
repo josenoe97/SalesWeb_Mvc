6368d868ed446601ae3c51ada6f5f80c08171cc2
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-08-14 08:30:54 -0300

    Second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,11 +1,39 @@
-﻿namespace SalesWebMvc.Models
+﻿using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SalesWebMvc.Models
 
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
+            
+        }
 
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public void RomeveSeller(Seller seller)
+        {
+            Sellers.Remove(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
