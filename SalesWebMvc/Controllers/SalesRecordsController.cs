2f0939d8a6f6f878922ec4ceb7f3a0caaba78a18
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-08-20 09:44:22 -0300

    Implementing simple search

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+             _salesRecordService = salesRecordService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime minDate, DateTime maxDate)
         {
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
             return View();
         }
 
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options => options.UseMySql(co
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
